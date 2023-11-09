cfd8dbb8c30ecb938043d5c2eca4c8f101c95687
Josh <jcastaneda85@csu.fullerton.edu>
2023-11-08 17:07:45 -0800

    First Program

--- hi.java
+++ hi.java
@@ -1,2 +1,16 @@
 // java
 // some changes and stuff foo
+
+import java.util.Scanner;
+
+public class hi {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("What do you want to say to the world?");
+
+        String input = scnr.nextLine();
+
+        System.out.println(input + " World!");
+    }
+}
